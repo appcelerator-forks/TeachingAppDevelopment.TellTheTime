1a21b34fc343853d4f6d64925aace2a1430f2e89
Claus Stilborg <cb@nebulators.com>
2014-10-25 21:27:51 +0200

    Added graphics for the clock

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,23 @@
 
+function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
+//randomDate(new Date(2012, 0, 1), new Date())
+
+
+
+
+// EVENT HANDLERS
+
 $.index.addEventListener("open", function(e){
 	console.log("Window has opened");
+	console.log("Random time generated:" + randomDate(new Date(2012, 0, 1), new Date()));
+	console.log("Random time generated:" + randomDate(new Date(2012, 0, 1), new Date()));
+	console.log("Random time generated:" + randomDate(new Date(2012, 0, 1), new Date()));
+	console.log("Random time generated:" + randomDate(new Date(2012, 0, 1), new Date()));
 });
+
 
 
 
